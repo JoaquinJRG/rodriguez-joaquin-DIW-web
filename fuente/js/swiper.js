3dbd84536ce6f38e2c39ebab64adbed559c7da6c
Joaquin <joaquinjrg01@gmail.com>
2024-02-11 23:59:28 +0100

    comentarios

---
+++ fuente/js/swiper.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+      //spaceBetween: 30,
+      direction: 'horizontal',
+      slidesPerView: 3,
+      loop: true,
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+          300: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        },
+    });
+    
